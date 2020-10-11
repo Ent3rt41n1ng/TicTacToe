16eef31518e30fa8687f248492d0ca7a8c423e41
Ent3rt41n1ng <66807730+Ent3rt41n1ng@users.noreply.github.com>
2020-10-11 08:13:25 +0700

    Update tictactoe.py
    Added new feature "VS Player"

--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,7 @@ board = {
     '6':'6', '7':'7', '8':'8'
 }
 
-def check_board():
+def check_board_bot():
     global turn
     global winning
     winning = False
@@ -31,7 +31,7 @@ def show_board(): # show the board of tic tac toe
     print('--|--|--')
     print(board['6'] + ' | ' + board['7'] + ' | ' + board['8'])
 
-def first_move(): # choose who will move first? bot or player?
+def first_move_bot(): # choose who will move first? bot or player?
     global turn
     print('Choosing who will take the first turn, Patience is necessary')
     time.sleep(1.5)
@@ -43,54 +43,142 @@ def first_move(): # choose who will move first? bot or player?
         print('Player will move first !')
         turn = 'X'
 
-first_move()
-
+print('Choose game mode :\n1. VS Player\n2. VS Bot')
 time.sleep(1.5)
+answer = input('Answer (in number) : ')
 
-show_board()
+while answer != '1' and answer != '2':
+    print('Your answer is not recognized! Please try again!')
+    print('Choose game mode :\n1. VS Player\n2. VS Bot')
+    time.sleep(1.5)
+    answer = input('Answer (in number) : ')
 
-# process
 options_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-for rounds in range(1, 10):
+if answer == '2':
+
+    first_move_bot()
+
     time.sleep(1.5)
-    if turn == 'O':
-        print('Bot\'s turn')
-        time.sleep(1.5)
-        bot = int(random.randint(0,8))
-        while bot not in options_list:
-            bot = random.randint(0,8)
-        board[str(bot)] = turn
-        options_list.remove(bot)   
-    else:
-        print('Player\'s turn')
-        time.sleep(1.5)
-        for options in options_list:
-            print(options)
-        player = input('Enter your choice based on the options above : ')
-        while int(player) not in options_list:
-            print('Your choice is not recognized! Please re-enter your choice!')
-            for options in options_list:
-                print(options)
-            player = input('Enter your choice based on the options above : ')   
-        board[player] = turn
-        options_list.remove(int(player))
 
     show_board()
-    check_board()
 
-    if winning == True:
-        break
+    # process
 
-    if turn == 'X':
-        turn = 'O'
-    else:
-        turn = 'X'   
-    time.sleep(1.5)
+    for rounds in range(1, 10):
+        time.sleep(1.5)
+        if turn == 'O':
+            print('Bot\'s turn')
+            time.sleep(1.5)
+            bot = int(random.randint(0,8))
+            while bot not in options_list:
+                bot = random.randint(0,8)
+            board[str(bot)] = turn
+            options_list.remove(bot)   
+        else:
+            print('Player\'s turn')
+            time.sleep(1.5)
+            for options in options_list:
+                print(options)
+            player = input('Enter your choice based on the options above : ')
+            while int(player) not in options_list:
+                print('Your choice is not recognized! Please re-enter your choice!')
+                for options in options_list:
+                    print(options)
+                player = input('Enter your choice based on the options above : ')   
+            board[player] = turn
+            options_list.remove(int(player))
+
+        show_board()
+        check_board_bot()
+
+        if winning == True:
+            break
+
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'   
     
+elif answer == '1':
 
+    player1 = input('Enter player 1\'s name : ')
+    player2 = input('Enter player 2\'s name : ')
 
-        
+    time.sleep(1.5)
+
+    def first_move_player(): # choose who will move first? bot or player?
+        global turn
+        print('Choosing who will take the first turn, Patience is necessary')
+        time.sleep(1.5)
+        choose = random.randint(1,2)
+        if choose == 1:
+            print('Player 1 will move first !')
+            turn = 'O'
+        else:
+            print('Player 2 will move first !')
+            turn = 'X'
+
+    def check_board_player():
+        global turn
+        global winning
+        winning = False
+        win = {'O':'Player 1', 'X':'Player 2'}
+        if board['0'] == board['1'] == board['2'] or board['3'] == board['4'] == board['5'] or board['6'] == board['7'] == board['8']:
+            print(win[turn] + ' won the match!')
+            winning = True
+        elif board['0'] == board['3'] == board['6'] or board['1'] == board['4'] == board['7'] or board['2'] == board['5'] == board['8']:
+            print(win[turn] + ' won the match!')
+            winning = True
+        elif board['0'] == board['4'] == board['8'] or board['2'] == board['4'] == board['6']:
+            print(win[turn] + ' won the match!')
+            winning = True
+
+    first_move_player()
+
+    time.sleep(1.5)
+
+    show_board()
+
+    for rounds in range(1, 10):
+        time.sleep(1.5)
+        if turn == 'O':
+            print('Player 1\'s turn')
+            time.sleep(1.5)
+            for options in options_list:
+                print(options)
+            player = input('Enter your choice based on the options above : ')
+            while int(player) not in options_list:
+                print('Your choice is not recognized! Please re-enter your choice!')
+                for options in options_list:
+                    print(options)
+                player = input('Enter your choice based on the options above : ')   
+            board[player] = turn
+            options_list.remove(int(player))  
+        else:
+            print('Player 2\'s turn')
+            time.sleep(1.5)
+            for options in options_list:
+                print(options)
+            player = input('Enter your choice based on the options above : ')
+            while int(player) not in options_list:
+                print('Your choice is not recognized! Please re-enter your choice!')
+                for options in options_list:
+                    print(options)
+                player = input('Enter your choice based on the options above : ')   
+            board[player] = turn
+            options_list.remove(int(player))
+
+        show_board()
+        check_board_player()
+
+        if winning == True:
+            break
+
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'   
             
 
 
